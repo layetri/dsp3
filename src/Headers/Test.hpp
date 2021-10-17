9732107c68fd75af06f64b8c54fb572655bf5ecb
layetri <daniel@mainstreamct.com>
2021-10-17 15:56:29 +0200

    Fixed assertion.

--- src/Headers/Test.hpp
+++ src/Headers/Test.hpp
@@ -7,7 +7,7 @@ struct Test {
     static void assertEqualResponse(float value, int place) {
         double assertion[] = {0.167772, 0, 0, 0, 0, 0, 0, 0, -0.971853, 0, 0, 0, 0, 0, 0, 0, -0.16305, 0, 0, 0, 0, 0, 0, 0, -0.0273552};
 
-        if(place < sizeof(assertion)) {
+        if(place < sizeof(assertion)/sizeof(*assertion)) {
             std::cout << "Output as desired: " << (int(value * 100000) == int(assertion[place] * 100000)) << std::endl << " [" << value << ", " << assertion[place] << "]" << std::endl;
         } else {
             std::cout << "Assertion failed: invalid data." << std::endl;
